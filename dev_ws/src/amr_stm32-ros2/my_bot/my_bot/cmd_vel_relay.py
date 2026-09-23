9244fc8ab65acf4f7f00296bf762235045993ed7
Tên_Tài_Khoản_GitHub <email_ng_nhp_github@gmail.com>
2026-09-23 19:14:19 +0700

    Thêm riêng thư mục dev_ws vào nhánh trống

---
+++ dev_ws/src/amr_stm32-ros2/my_bot/my_bot/cmd_vel_relay.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Relay node: đảo dấu angular.z từ /cmd_vel_raw sang /cmd_vel."""
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+
+class CmdVelRelay(Node):
+    def __init__(self):
+        super().__init__('cmd_vel_relay')
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.sub = self.create_subscription(Twist, '/cmd_vel_raw', self.callback, 10)
+
+    def callback(self, msg: Twist):
+        out = Twist()
+        out.linear.x = msg.linear.x
+        out.linear.y = msg.linear.y
+        out.linear.z = msg.linear.z
+        out.angular.x = msg.angular.x
+        out.angular.y = msg.angular.y
+        out.angular.z = -msg.angular.z  # đảo chiều quay
+        self.pub.publish(out)
+
+
+def main():
+    rclpy.init()
+    node = CmdVelRelay()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
